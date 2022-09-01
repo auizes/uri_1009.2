7025e178ecf1066642f8a7fbd8b0d5b7130734d7
gustavo.ssilva88 <gustavo.ssilva88@L130570886W10-1>
2022-09-01 14:01:59 -0300

    Adicionar arquivos de projeto.

---
+++ uri_1009.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1009._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
